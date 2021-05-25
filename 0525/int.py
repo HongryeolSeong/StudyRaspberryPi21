9f43b87972ee43ff51164b2f3a5df642eec10be8
ghd0276 <ghd0276@naver.com>
2021-05-25 17:13:45 +0900

    5/25 주석 추가

--- 0525/int.py
+++ 0525/int.py
@@ -9,13 +9,15 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(switch, GPIO.IN, pull_up_down = GPIO.PUD_DOWN)	#내부풀다운 사용
 
 def swBlink(channel):	# callback 함수
-	global flag
+	global flag 	# 전역변수를 함수 내 사용시 선언 필요
 	if flag == False:
 		print("interrupt")
 		flag = True
 	else:
 		flag = False
+		
 #인터럽터핀에 라이징신호가 인가되면 콜백함수로 리턴되어 실행된다.
+#하드웨어가 스위치 눌림 신호(라이징)를 감지
 GPIO.add_event_detect(switch, GPIO.RISING, callback=swBlink)
 
 try:
