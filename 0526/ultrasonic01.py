56610f5ddf77d0ba7d5289e72bc865145a5242ae
ghd0276 <ghd0276@naver.com>
2021-05-26 19:18:53 +0900

    5/26 주석

--- 0526/ultrasonic01.py
+++ 0526/ultrasonic01.py
@@ -20,10 +20,10 @@ try:
 		time.sleep(0.00001)
 		GPIO.output(triggerPin, GPIO.HIGH)
 
-		while GPIO.input(echoPin) == 0:
-			start = time.time()
-		while GPIO.input(echoPin) == 1:
-			stop = time.time()
+		while GPIO.input(echoPin) == 0: #초음파 전송이 끝나는 전송시간
+			start = time.time()         #을 저장
+		while GPIO.input(echoPin) == 1: #초음파 수신이 완료된 때 시간
+			stop = time.time()          #을 저장
 
 		rtTotime = stop - start
 		distance = rtTotime * 34000 / 2
