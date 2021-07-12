7c12b1a79ab83a6666c94ab976ddd61248f606e6
HongryeolSeong <ghd0276@naver.com>
2021-07-12 17:30:02 +0900

    7/12 Python 기본 문법 학습 - 클래스까지

---
+++ 0712/Person.py
@@ -0,0 +1,10 @@
+class Person(object):
+    total = 0
+
+    def __init__(self, name, age):  # 기본 생성자 : (self)
+                                    # 생성자 오버로딩
+        self.name = name
+        self.age = age
+    
+    def getAge(self):
+        return self.age
