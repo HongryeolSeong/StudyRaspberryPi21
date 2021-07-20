17b4f36463021b50a63e05b98fb9d717a07a03f3
HongryeolSeong <ghd0276@naver.com>
2021-07-20 15:04:11 +0900

    7/20 영상 처리 - 영상 자르기

--- 0719_20/opencv_mov03.py
+++ 0719_20/opencv_mov03.py
@@ -2,19 +2,22 @@ import cv2
 import numpy as np
 
 # 카메라 기본 틀
-# 흑백 처리
+# 영상 자르기
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
 
 while True:
     ret, frame = cap.read()
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    h, w, c = frame.shape
+    crop = frame[:, :int(w/2)]
 
     if ret != True: break
 
     cv2.imshow('RealTime CAM', frame)   # 원본
-    cv2.imshow('Gray Result', gray)     # 수정본
+    cv2.imshow('Cropped CAM', crop)     # 수정본
+
 
     if cv2.waitKey(1) == ord('q'): break
 
