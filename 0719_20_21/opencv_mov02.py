1fbfdc0ede1e2dc1863ee063627f5653d04ea1c4
HongryeolSeong <ghd0276@naver.com>
2021-07-30 10:45:43 +0900

    7/29 OpenCV 주석 주성

--- 0719_20_21/opencv_mov02.py
+++ 0719_20_21/opencv_mov02.py
@@ -1,21 +1,20 @@
 import cv2
 import numpy as np
 
-# 카메라 기본 틀
-# 흑백 처리
+# 회색조 처리
 cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 0)
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # 회색조 화면 생성
 
     if ret != True: break
 
-    cv2.imshow('RealTime CAM', frame)   # 원본
-    cv2.imshow('Gray Result', gray)     # 수정본
+    cv2.imshow('RealTime CAM', frame)   # 원본 출력
+    cv2.imshow('Gray Result', gray)     # 수정본 출력
 
     if cv2.waitKey(1) == ord('q'): break
 
--- 0719_20_21/opencv_mov04_2.py
+++ 0719_20_21/opencv_mov04_2.py
@@ -1,8 +1,7 @@
 import cv2
 import numpy as np
 
-# 영상 블러 처리 + 영상 노이즈 처리
-# 영상 합치기
+# 영상 합치기(원본 + 노이즈 수정본)
 cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
@@ -10,7 +9,6 @@ cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 360)
 while True:
     ret, frame = cap.read()
     frame = cv2.flip(frame, 0)
-    blur = cv2.blur(frame, (50, 50))    # 블러 처리
 
     h, w, c = frame.shape
     noise = np.uint8(np.random.normal(loc=0, scale=50.0, size=[h, w, c]))
@@ -18,13 +16,8 @@ while True:
 
     if ret != True: break
 
-    #total = cv2.hconcat([frame, noised])  # 영상 합치기
-    #cv2.imshow('Concat', total)           # 합친 영상 출력
-
-    cv2.imshow('RealTime CAM', frame)   # 원본
-    cv2.imshow('Blurred CAM', blur)     # 수정본
-    cv2.imshow('Noised CAM', noised)    # 수정본
-
+    total = cv2.hconcat([frame, noised])  # 영상 합치기
+    cv2.imshow('Concat', total)           # 합친 영상 출력
 
     if cv2.waitKey(1) == ord('q'): break
 
